089ca665bae9ad4b1a78864af1b0f8f4ca296c8d
useUK <urauzuk@gmail.com>
2016-11-05 22:26:20 +0200

    Add files via upload

--- Task1.py
+++ Task1.py
@@ -1,19 +1,19 @@
 def miniCalck (x,y,z):
+	float c
 	if z == "+":
 		c = x + y
-	elif z == "-":
+	if z == "-":
 		c = x - y 
-	elif z == "*":
+	if z == "*":
 		c = x * y
-	elif z == "/":
+	if z == "/":
 		c = x / y
 	else 
 		print('error baka')
-	return c
-х = int(input("Enter your value 1:" )
-y = int(input("Enter your value 2:" )
-z = input("Enter your value 1:" )
-Answer = miniCalck (x,y,z)
-print(" Your answer = ", Answer) 
-
+	return '%+2f' c
+x = float(input("Enter your value 1:" ))
+y = float(input("Enter your value 2:" ))
+z = input("Enter your symbal:" )
+answer = miniCalck (x,y,z)
+print(" Your answer = ", answer) 
 
