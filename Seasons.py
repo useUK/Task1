cc0e0a9fef28206f878790993286d28032e5a4e9
useUK <urauzuk@gmail.com>
2016-11-08 22:17:50 +0200

    Update Seasons.py

--- Seasons.py
+++ Seasons.py
@@ -2,14 +2,14 @@
 def month (x):
 	if (3<=x<=5): 
 		return 'vesna'
-	elif (x == 12) or (x == 1) or (x == 2):
+	elif (x == 12) or (1<=x<=2):
 		return 'winter' 
 	elif (6<=x<=8): 
 		return 'summer'
 	elif (9<=x<=11): 
 		return 'autmn'
-	else (1<=x<=12): 
-		print ('error')
+	else: 
+		return 'not exist this mounth'
 
 x = int ( input ("Enter your value mounth:" ) )
 
